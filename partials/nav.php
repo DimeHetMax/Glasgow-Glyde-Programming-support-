5237150df02ecb511980642b3645de3a88dea755
Maksym Dimekhin <20361352@myclyde.ac.uk>
2026-02-10 14:41:16 +0000

    working with tailwind

---
+++ partials/nav.php
@@ -0,0 +1,21 @@
+<navbar>
+      <div class='w-full py-3 border-b'>
+
+        <div class='flex justify-between px-20 items-center font-semibold'>
+          <div>
+            <a href="./index.php"> <h1 class="text-2xl">LOGO</h1></a>
+          </div>
+          <div class='flex xl:gap-10 md:gap-8  gap-2'>
+            <a href="./index.php">Home</a>
+            <a href="#work">Work</a>
+            <a href="./students.php">Student</a>
+            <a href="">Services</a>
+            <a href="">Testimonial</a>
+          </div>
+          <div>
+            <button class='py-2 px-6 bg-black text-white rounded-3xl font-semibold'>Contact</button>
+          </div>
+        </div>
+        
+      </div>
+    </navbar>
